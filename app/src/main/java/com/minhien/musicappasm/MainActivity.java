6e69ca652c49e2742b4a26174139d2b98bc3dd47
Admin <vuminhhien05@gmail.com>
2021-07-15 22:32:04 +0700

    Update

--- app/src/main/java/com/minhien/musicappasm/MainActivity.java
+++ app/src/main/java/com/minhien/musicappasm/MainActivity.java
@@ -1,14 +1,27 @@
 package com.minhien.musicappasm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.cv_3107_2);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,PlayMusic.class);
+                startActivity(i);
+            }
+        });
     }
 }
