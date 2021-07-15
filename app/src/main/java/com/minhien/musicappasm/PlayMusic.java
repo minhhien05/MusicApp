2ebba2e65fb6855444302ab21f86d1ea67f3e759
minhhien05 <hienvmps13917@fpt.edu.vn>
2021-07-15 22:22:10 +0700

    ASM do ban

---
+++ app/src/main/java/com/minhien/musicappasm/PlayMusic.java
@@ -0,0 +1,14 @@
+package com.minhien.musicappasm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_music);
+    }
+}
