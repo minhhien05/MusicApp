b1779f28617755943789beddc1e203838cde7687
Admin <vuminhhien05@gmail.com>
2021-07-16 08:56:36 +0700

    Update

--- app/src/main/java/com/minhien/musicappasm/MainActivity.java
+++ app/src/main/java/com/minhien/musicappasm/MainActivity.java
@@ -6,17 +6,18 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    CardView cardView;
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cardView = findViewById(R.id.cv_3107_2);
+        relativeLayout = findViewById(R.id.rlt_3107_2);
 
-        cardView.setOnClickListener(new View.OnClickListener() {
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this,PlayMusic.class);
--- app/src/main/java/com/minhien/musicappasm/PlayMusic.java
+++ app/src/main/java/com/minhien/musicappasm/PlayMusic.java
@@ -2,13 +2,29 @@ package com.minhien.musicappasm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import soup.neumorphism.NeumorphImageView;
 
 public class PlayMusic extends AppCompatActivity {
+
+    NeumorphImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+
+        back = findViewById(R.id.imageMenu);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PlayMusic.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
